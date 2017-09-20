36982808684e94d2471bcc3b0edc68f8a108c713
Joseph Burger <candyapplecorn@gmail.com>
2017-09-19 19:08:53 -0700

    session form container probably works now

--- frontend/components/session_form_container.js
+++ frontend/components/session_form_container.js
@@ -2,8 +2,8 @@ import { connect } from 'react-redux'
 import SessionForm from './session_form'
 import { signup, login } from '../actions/session_actions'
 
-const getFormType = ({ match: { path } }) =>
-  /login/.test(path) ? 'login' : 'signup'
+const getFormType = ({ location: { pathname } }) =>
+  /login/.test(pathname) ? 'login' : 'signup'
 ;
 
 const getFormAction = ownProps =>
@@ -18,11 +18,13 @@ const NEW_USER = { username: '', email: '', password: '' };
 
 const mapStateToProps = state => ({
   user: state.session.currentUser || NEW_USER,
-  formType: getFormType(ownProps)
+  loggedIn: Boolean(state.session.currentUser),
+  formType: getFormType(ownProps),
+  errors: state.errors.session.errors
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-    submitForm: createAction(getFormAction(ownProps), dispatch)
+  submitForm: createAction(getFormAction(ownProps), dispatch)
 });
 
 const SessionFormContainer = connect(
