7960129e367b1bd7eb795bd0a41f21d977c53663
Joseph Burger <candyapplecorn@gmail.com>
2017-09-22 09:59:07 -0700

    added fontawesome and edit/delete buttons to story show page for author only

--- app/views/api/stories/show.json.jbuilder
+++ app/views/api/stories/show.json.jbuilder
@@ -1,5 +1,7 @@
 json.partial! 'api/stories/story', story: @story
 
+json.set! :current_is_author, !!(current_user && @story.author == current_user)
+
 json.set! :comments do
   json.array! @story.comments do |comment|
     json.body = comment.body
--- frontend/components/story_show.jsx
+++ frontend/components/story_show.jsx
@@ -25,10 +25,40 @@ class StoryShow extends React.Component {
 
     return (
       <main className="story-show">
+        {this.buttonList()}
         <h1 className="story-title">{title}</h1>
         <section className="story-body">{ ReactHtmlParser(body) }</section>
       </main>
     );
+  }
+
+  buttonList(){
+    const { story: { current_is_author } } = this.props
+    if (!Boolean(current_is_author))
+      return (null);
+
+    return (
+        <div className="story-show-author-buttons">
+          <button onClick={this.goToEdit.bind(this)}>
+            <i className="fa fa-pencil" aria-hidden="false"></i>
+            Edit
+          </button>
+          <button onClick={this.deleteThis.bind(this)}>
+            <i className="fa fa-trash" aria-hidden="true"></i>
+            Delete
+          </button>
+        </div>
+    );
+  }
+
+  goToEdit(event){
+    event.preventDefault()
+    this.props.history.push(`/stories/${this.props.match.params.id}/edit`)
+  }
+
+  deleteThis(event){
+    event.preventDefault()
+    this.props.delete().then(() => this.props.history.push('/'))
   }
 }
 
