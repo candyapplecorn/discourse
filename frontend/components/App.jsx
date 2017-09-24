41d0358de57dbb80589eabf0a2c22a75af9fcd48
Joseph Burger <candyapplecorn@gmail.com>
2017-09-23 17:37:16 -0700

    Created and styled a footer which sticks to the bottom of the screen

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -7,24 +7,29 @@ import SessionFormContainer from './session_form_container';
 import StoryFormContanier from './story_form_container';
 import StoryShowContainer from './story_show_container';
 import IndexContainer from './index_container';
+import Footer from './footer'
 
 // Todo: Make header a component
 const App = () => (
-  <div>
+  <div className='site'>
     <header>
       <Link to="/stories/new">Write Something</Link>
       <h1><Link to="/" className="home-link">Write it Up</Link></h1>
       <AuthenticationContainer />
     </header>
 
-    <Switch>
-      <Route exact path="/" component={IndexContainer} />
-      <AuthRoute path="/login"  component={SessionFormContainer} />
-      <AuthRoute path="/signup" component={SessionFormContainer} />
-      <ProtectedRoute exact path="/stories/new" component={StoryFormContanier} />
-      <ProtectedRoute exact path="/stories/:id/edit" component={StoryFormContanier} />
-      <Route exact path="/stories/:id" component={StoryShowContainer} />
-    </Switch>
+    <main className="site-content">
+      <Switch>
+        <Route exact path="/" component={IndexContainer} />
+        <AuthRoute path="/login"  component={SessionFormContainer} />
+        <AuthRoute path="/signup" component={SessionFormContainer} />
+        <ProtectedRoute exact path="/stories/new" component={StoryFormContanier} />
+        <ProtectedRoute exact path="/stories/:id/edit" component={StoryFormContanier} />
+        <Route exact path="/stories/:id" component={StoryShowContainer} />
+      </Switch>
+      </main>
+
+    <Footer />
   </div>
 );
 
---
+++ frontend/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = props => (
+  <footer className='footer'>
+    <a target="_blank" href="https://github.com/candyapplecorn/discourse">
+      <i className="fa fa-github" aria-hidden="true"></i>
+    </a>
+  </footer>
+);
+
+export default Footer
