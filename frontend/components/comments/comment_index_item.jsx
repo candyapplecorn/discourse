1c07897ec7a038a5d2073dd946a233f7158c4c01
Joseph Burger <candyapplecorn@gmail.com>
2017-09-26 12:15:43 -0700

    added authors' usernames to comments, but only if current user isn't the author. otherwise the authorship buttons show up.

--- frontend/components/comments/comment_index_item.jsx
+++ frontend/components/comments/comment_index_item.jsx
@@ -8,6 +8,8 @@ class CommentIndexItem extends React.Component {
     this.props.editComment(this.props.comment.id)
   }
   render(){
+    const { comment: { currentIsAuthor, author: { username } }} = this.props
+
     const authorActions = (
         <button className="edit-comment" onClick={this.handleClick.bind(this)}>
           <i className="fa fa-pencil" aria-hidden="false"></i>
@@ -15,12 +17,18 @@ class CommentIndexItem extends React.Component {
         </button>
     );
 
+    const authorDetails = (
+      <em>
+        {username}
+      </em>
+    )
+
     return (
       <div>
         <p>
           {this.props.comment.body}
         </p>
-        {this.props.comment.currentIsAuthor && authorActions}
+        {currentIsAuthor ? authorActions : authorDetails}
       </div>
     )
   }
