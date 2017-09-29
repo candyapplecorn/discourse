71e6650e39d71c2b16793b3340eb6427757299af
Joseph Burger <candyapplecorn@gmail.com>
2017-09-29 13:20:47 -0700

    preparing app for production by separating developer tools into developer jurisdictions

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,14 +4,20 @@ import rootReducer from '../reducers/root_reducer';
 import thunk from '../middleware/thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+const middlewares = [thunk]
+
+if (process.env.NODE_ENV !== 'production'){
+  const { logger } = require('redux-logger')
+  middlewares.push(logger)
+}
+
 const configureStore = (preloadedState = {}) =>
   createStore(
     rootReducer,
     preloadedState,
     composeWithDevTools(
       applyMiddleware(
-        thunk,
-        logger
+        ... middlewares
       )
     )
   )
